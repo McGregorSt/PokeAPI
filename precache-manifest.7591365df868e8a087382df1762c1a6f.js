61fa8924612c2e65a4d5415dcf0d8c546e607322
Grzegorz Stasiak <stasiak.gregor@gmail.com>
2020-04-17 09:28:08 +0200

    Updates

---
+++ precache-manifest.7591365df868e8a087382df1762c1a6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc138d14a529fcc1ce870a16693fda8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5055c6a352b1cc666bda",
+    "url": "/static/css/main.e0fbffb1.chunk.css"
+  },
+  {
+    "revision": "ab78c054dd77cb212148",
+    "url": "/static/js/2.064c9340.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.064c9340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5055c6a352b1cc666bda",
+    "url": "/static/js/main.734c67ec.chunk.js"
+  },
+  {
+    "revision": "0c973e6fd06ebb48a215",
+    "url": "/static/js/runtime-main.8d3fb40e.js"
+  }
+]);
